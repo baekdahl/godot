c22aa02f7ac50396dcbfd028d9578240f8912105
clayjohn <claynjohn@gmail.com>
2019-08-18 21:50:33 -0700

    check if skeleton texture is already allocated before reallocating

--- drivers/gles2/rasterizer_storage_gles2.cpp
+++ drivers/gles2/rasterizer_storage_gles2.cpp
@@ -3457,6 +3457,8 @@ RID RasterizerStorageGLES2::skeleton_create() {
 
 	Skeleton *skeleton = memnew(Skeleton);
 
+	glGenTextures(1, &skeleton->tex_id);
+
 	return skeleton_owner.make_rid(skeleton);
 }
 
@@ -3474,7 +3476,6 @@ void RasterizerStorageGLES2::skeleton_allocate(RID p_skeleton, int p_bones, bool
 	skeleton->use_2d = p_2d_skeleton;
 
 	if (config.float_texture_supported) {
-		glGenTextures(1, &skeleton->tex_id);
 
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, skeleton->tex_id);
